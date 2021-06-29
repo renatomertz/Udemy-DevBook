614e92dd648f1d448025fe2ef862d78c2d61f9bc
Renato Mertz <renatomertz@gmail.com>
2021-06-29 10:52:21 -0300

    User structure and database script

---
+++ api/src/model/User.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+//User
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Name     string    `json:"name,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Password string    `json:"password,omitempty"`
+	Created  time.Time `json:"created,omitempty"`
+}
